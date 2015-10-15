8a42825172575153b9b439a17747d70e24f258de
gnaix <990828785@qq.com>
2015-10-15 23:41:42 +0800

    update

--- app/src/main/java/com/mlegeb/remote/common/Update.java
+++ app/src/main/java/com/mlegeb/remote/common/Update.java
@@ -1,5 +1,7 @@
 package com.mlegeb.remote.common;
 
+import android.os.AsyncTask;
+
 /**
  * 名称: Update
  * 描述: 版本更新组件
@@ -7,5 +9,9 @@ package com.mlegeb.remote.common;
  * @author xiangqing.xue
  * @date 15/10/14
  */
-public class Update {
+public class Update extends AsyncTask<Void, Integer, Boolean>{
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        return null;
+    }
 }
